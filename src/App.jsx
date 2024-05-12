54fe52cf90cde4f39bbbf6c55a206208e3cae1b8
00jackson <95626883+00jackson@users.noreply.github.com>
2024-05-12 14:31:17 +0530

    Design the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Work from "./components/Work";
+import Location from "./components/Location";
+import GetStarted from "./components/GetStarted";
+import Testimonials from "./components/Testimonials";
+
+useState
+
 const App = () => {
+
   return (
-    <div className=" h-screen flex justify-center items-center font-semibold text-3xl">Happy Coding</div>
+    <>
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Work/>
+    <Location/>
+    <GetStarted/>
+    <Testimonials/>
+    </>
   )
 }
 
-export default App
+export default App 
