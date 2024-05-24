d13219cd87cf0ba26807926a2caac1f27cdb1052
00jackson <95626883+00jackson@users.noreply.github.com>
2024-05-24 21:50:14 +0530

    Made responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,37 +1,37 @@
 
 const About = () => {
   return <section className="text-xl ">
-  <div className="  w-full h-[1100px] flex flex-col items-center justify-center relative  ">
-          <div className=" h-[60%] absolute  top-[5.8vw] left-[16.5vw] -z-10" ><img src="https://storyvord.com/img/camera-img1.svg" className="w-[11vw] " alt="" /></div> 
-          <div className="relative w-[85%] h-64  flex  space-x-4 ">
-          <div className=" relative w-[25vw]   flex  font-bold text-6xl font-barlow right-[5vw] ">ABOUT <br /> STORYVORD </div>
+    <div className="  w-full h-[1100px] flex flex-col items-center justify-center relative  ">
+      <div className=" h-[60%] absolute  top-[5.8vw] left-[16.5vw] -z-10" ><img src="https://storyvord.com/img/camera-img1.svg" className="w-[11vw] " alt="" /></div>
+      <div className="relative w-[85%] h-64  flex  space-x-4 ">
+        <div className=" relative w-[25vw]   flex  font-bold text-6xl font-barlow right-[5vw] ">ABOUT <br /> STORYVORD </div>
         <div className="  relative w-[60vw] h-80 top-[4vw] bg-[white]  py-[0vw] -mt-[4vw] p-[2vw]">
-           <h3 className=" text-[1.5vw] flex flex-row"><span className=" relative flex flex-row  mt-[3.3vw] h-[0.3vw] w-[6.3vw] mx-[1vw] bg-gradient-to-r from-[#012284]  via-[#1AA36E] to-[#25D865]" ></span> <b className="font-Josefin-Sans mt-[2vw] ">WE ARE STORYVORD, COMMUNITY OF CREATORS WORLDWIDE</b></h3>
-                  <br />
-            <p className="text-[1.3vw] font-Josefin-Sans px-[2vw] leading-none  mt-[1vw]">We connect global filmmakers like writers, designers, video producers, animators, audio engineers, and photographers to connect with corporations, production agencies and brands in 18 countries.</p>
-            <br />
-            <p className="text-[1.3vw] font-Josefin-Sans px-[2vw] leading-none  mt-[0.5vw]">Our AI-powered process and global content-creator community reduce the cost of content creation by up to 50% through remote execution.</p>
+          <h3 className=" text-[1.5vw] flex flex-row"><span className=" relative flex flex-row  mt-[3.3vw] h-[0.3vw] w-[6.3vw] mx-[1vw] bg-gradient-to-r from-[#012284]  via-[#1AA36E] to-[#25D865]" ></span> <b className="font-Josefin-Sans mt-[2vw] ">WE ARE STORYVORD, COMMUNITY OF CREATORS WORLDWIDE</b></h3>
+          <br />
+          <p className="text-[1.3vw] font-Josefin-Sans px-[2vw] leading-none  mt-[1vw]">We connect global filmmakers like writers, designers, video producers, animators, audio engineers, and photographers to connect with corporations, production agencies and brands in 18 countries.</p>
+          <br />
+          <p className="text-[1.3vw] font-Josefin-Sans px-[2vw] leading-none  mt-[0.5vw]">Our AI-powered process and global content-creator community reduce the cost of content creation by up to 50% through remote execution.</p>
         </div>
-    
-       </div>
-                    <div><img src="https://storyvord.com/img/video-bg.png" alt="" /></div>
-     </div>  
+
+      </div>
+      <div><img src="https://storyvord.com/img/video-bg.png" alt="" /></div>
+    </div>
     {/* services */}
-        <div className="  relative  w-full h-[46vw]   flex justify-center">
-           <div className=" relative  flex w-[90%] justify-center items-center-mt-[1.5vw]">
-                <div className="  relative  w-[18vw] h-[30vw] mx-[3vw]  -bottom-[5vw]">   <div className="grayscale opacity-5  relative left-[3vw] ">< img src="https://storyvord.com/img/service-icon1.svg"  className="w-[9vw]"alt="" /></div> n <div className=" overflow-hidden group relative left-[0vw] -top-[7vw] bg-[rgb(255,_255,_255)] rounded-[1vw] w-[7vw] h-[7vw] [box-shadow:-12px_11px_18px_rgba(0,_0,_0,_.15)] flex flex-col justify-center items-center before:absolute before:top-full before:left-0 before:h-full before:w-full before:bg-custom-gradient before:transition-transform before:duration-300 hover:before:-translate-y-full "><img src="https://storyvord.com/img/service-icon1.svg" className=" w-[5vw] relative z-10 block transition-colors duration-300 group hover:brightness-[100]" alt="" /></div><h1 className=" relative -top-[4vw] text-[1.5vw] font-bold font-barlow">EFFICIENT </h1> <h3 className=" relative -top-[3vw] text-[1.2vw] leading-[1.5vw] pr-[3vw] font-Josefin-Sans">We make video production simple and affordable. Our process is smooth and time-efficient. We can shoot multiple country videos at the same time.</h3></div>
-                <div className="  relative  w-[18vw] h-[30vw] mx-[3vw]">   <div className="grayscale opacity-5 relative left-[3vw]"><img src="https://storyvord.com/img/service-icon2.svg" className="w-[9vw]" alt="" /></div>  n <div className=" overflow-hidden group relative left-[0vw] -top-[7vw] bg-[rgb(255,_255,_255)] rounded-[1vw] w-[7vw] h-[7vw] [box-shadow:-12px_11px_18px_rgba(0,_0,_0,_.15)] flex flex-col justify-center items-center before:absolute before:top-full before:left-0 before:h-full before:w-full before:bg-custom-gradient before:transition-transform before:duration-300 hover:before:-translate-y-full"><img src="https://storyvord.com/img/service-icon2.svg" className=" w-[5vw] relative z-10 block transition-colors duration-300 group hover:brightness-[100]  " alt="" /></div> <h1 className="relative -top-[4vw] text-[1.5vw] font-bold font-barlow ">TRANSPARENCY</h1><h3 className=" relative -top-[3vw] text-[1.2vw] leading-[1.5vw] pr-[3vw] font-Josefin-Sans">Approval at each stage with real-time monitoring of all your projects in progress with our transparent delivery system.</h3></div>
-                <div className="  relative  w-[18vw] h-[30vw] mx-[3vw] -bottom-[5vw]">   <div className=" grayscale opacity-5 relative left-[3vw]"><img src="https://storyvord.com/img/service-icon3.svg" className="w-[9vw]" alt="" /></div> n <div className="overflow-hidden group relative left-[0vw] -top-[7vw] bg-[rgb(255,_255,_255)] rounded-[1vw] w-[7vw] h-[7vw] [box-shadow:-12px_11px_18px_rgba(0,_0,_0,_.15)] flex flex-col justify-center items-center before:absolute before:top-full before:left-0 before:h-full before:w-full before:bg-custom-gradient before:transition-transform before:duration-300 hover:before:-translate-y-full"><img src="https://storyvord.com/img/service-icon3.svg" className=" w-[5vw] relative z-10 block transition-colors duration-300 group hover:brightness-[100] " alt="" /></div><h1 className=" relative -top-[4vw] text-[1.5vw] font-bold font-barlow">GLOBAL COMMUNITY</h1><h3 className=" relative -top-[3vw] text-[1.2vw] leading-[1.5vw]  pr-[3vw] font-Josefin-Sans">Our global community of creators share real-time collaboration, pool resources like share equipment and leverage each other's expertise.</h3></div>
-                <div className="  relative  w-[18vw] h-[30vw] mx-[3vw]">   <div className=" grayscale opacity-5 relative left-[3vw]"><img src="https://storyvord.com/img/service-icon4.svg" className="w-[9vw]" alt="" /></div> n <div className=" overflow-hidden group relative left-[0vw] -top-[7vw] bg-[rgb(255,_255,_255)] rounded-[1vw] w-[7vw] h-[7vw] [box-shadow:-12px_11px_18px_rgba(0,_0,_0,_.15)] flex flex-col justify-center items-center before:absolute before:top-full before:left-0 before:h-full before:w-full before:bg-custom-gradient before:transition-transform before:duration-300 hover:before:-translate-y-full"><img src="https://storyvord.com/img/service-icon4.svg" className=" w-[5vw] relative z-10 block transition-colors duration-300 group hover:brightness-[100] " alt="" /></div><h1 className=" relative -top-[4vw] text-[1.5vw] font-bold font-barlow">INSIGHTFUL</h1> <h3 className=" relative -top-[3vw] text-[1.2vw] leading-[1.5vw] pr-[3vw]  font-Josefin-Sans">Our data-based insight assists in making strategic business decisions and improving ROI by using facts, metrics, and reports.</h3></div>
+    <div className="  relative  w-full h-[46vw]   flex justify-center">
+      <div className=" relative  flex w-[90%] justify-center items-center-mt-[1.5vw]">
+        <div className="  relative  w-[18vw] h-[30vw] mx-[3vw]  -bottom-[5vw]">   <div className="grayscale opacity-5  relative left-[3vw] ">< img src="https://storyvord.com/img/service-icon1.svg" className="w-[9vw]" alt="" /></div> n <div className=" overflow-hidden group relative left-[0vw] -top-[7vw] bg-[rgb(255,_255,_255)] rounded-[1vw] w-[7vw] h-[7vw] [box-shadow:-12px_11px_18px_rgba(0,_0,_0,_.15)] flex flex-col justify-center items-center before:absolute before:top-full before:left-0 before:h-full before:w-full before:bg-custom-gradient before:transition-transform before:duration-300 hover:before:-translate-y-full "><img src="https://storyvord.com/img/service-icon1.svg" className=" w-[5vw] relative z-10 block transition-colors duration-300 group hover:brightness-[100]" alt="" /></div><h1 className=" relative -top-[4vw] text-[1.5vw] font-bold font-barlow">EFFICIENT </h1> <h3 className=" relative -top-[3vw] text-[1.2vw] leading-[1.5vw] pr-[3vw] font-Josefin-Sans">We make video production simple and affordable. Our process is smooth and time-efficient. We can shoot multiple country videos at the same time.</h3></div>
+        <div className="  relative  w-[18vw] h-[30vw] mx-[3vw]">   <div className="grayscale opacity-5 relative left-[3vw]"><img src="https://storyvord.com/img/service-icon2.svg" className="w-[9vw]" alt="" /></div>  n <div className=" overflow-hidden group relative left-[0vw] -top-[7vw] bg-[rgb(255,_255,_255)] rounded-[1vw] w-[7vw] h-[7vw] [box-shadow:-12px_11px_18px_rgba(0,_0,_0,_.15)] flex flex-col justify-center items-center before:absolute before:top-full before:left-0 before:h-full before:w-full before:bg-custom-gradient before:transition-transform before:duration-300 hover:before:-translate-y-full"><img src="https://storyvord.com/img/service-icon2.svg" className=" w-[5vw] relative z-10 block transition-colors duration-300 group hover:brightness-[100]  " alt="" /></div> <h1 className="relative -top-[4vw] text-[1.5vw] font-bold font-barlow ">TRANSPARENCY</h1><h3 className=" relative -top-[3vw] text-[1.2vw] leading-[1.5vw] pr-[3vw] font-Josefin-Sans">Approval at each stage with real-time monitoring of all your projects in progress with our transparent delivery system.</h3></div>
+        <div className="  relative  w-[18vw] h-[30vw] mx-[3vw] -bottom-[5vw]">   <div className=" grayscale opacity-5 relative left-[3vw]"><img src="https://storyvord.com/img/service-icon3.svg" className="w-[9vw]" alt="" /></div> n <div className="overflow-hidden group relative left-[0vw] -top-[7vw] bg-[rgb(255,_255,_255)] rounded-[1vw] w-[7vw] h-[7vw] [box-shadow:-12px_11px_18px_rgba(0,_0,_0,_.15)] flex flex-col justify-center items-center before:absolute before:top-full before:left-0 before:h-full before:w-full before:bg-custom-gradient before:transition-transform before:duration-300 hover:before:-translate-y-full"><img src="https://storyvord.com/img/service-icon3.svg" className=" w-[5vw] relative z-10 block transition-colors duration-300 group hover:brightness-[100] " alt="" /></div><h1 className=" relative -top-[4vw] text-[1.5vw] font-bold font-barlow">GLOBAL COMMUNITY</h1><h3 className=" relative -top-[3vw] text-[1.2vw] leading-[1.5vw]  pr-[3vw] font-Josefin-Sans">Our global community of creators share real-time collaboration, pool resources like share equipment and leverage each others expertise.</h3></div>
+        <div className="  relative  w-[18vw] h-[30vw] mx-[3vw]">   <div className=" grayscale opacity-5 relative left-[3vw]"><img src="https://storyvord.com/img/service-icon4.svg" className="w-[9vw]" alt="" /></div> n <div className=" overflow-hidden group relative left-[0vw] -top-[7vw] bg-[rgb(255,_255,_255)] rounded-[1vw] w-[7vw] h-[7vw] [box-shadow:-12px_11px_18px_rgba(0,_0,_0,_.15)] flex flex-col justify-center items-center before:absolute before:top-full before:left-0 before:h-full before:w-full before:bg-custom-gradient before:transition-transform before:duration-300 hover:before:-translate-y-full"><img src="https://storyvord.com/img/service-icon4.svg" className=" w-[5vw] relative z-10 block transition-colors duration-300 group hover:brightness-[100] " alt="" /></div><h1 className=" relative -top-[4vw] text-[1.5vw] font-bold font-barlow">INSIGHTFUL</h1> <h3 className=" relative -top-[3vw] text-[1.2vw] leading-[1.5vw] pr-[3vw]  font-Josefin-Sans">Our data-based insight assists in making strategic business decisions and improving ROI by using facts, metrics, and reports.</h3></div>
 
 
-             
-           </div>         
-       </div>
-       </section>;
-        
-    
-       
+
+      </div>
+    </div>
+  </section>;
+
+
+
 };
 
 
